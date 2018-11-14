5d25d658b99578c3c1b8db603f8e3f36b4dae3de
OscarWilson <ozziew007@yahoo.co.uk>
2018-11-14 13:38:39 +0000

    Functional AStar

--- AStarAlgorithm.py
+++ AStarAlgorithm.py
@@ -17,6 +17,16 @@ class Cell():
         self.h = 0
         self.f = 0
         self.visited = False
+    def Display_Details(self):
+        print("Cell ", self.x ,",", self.y )
+        print("Holds value - ", self.value)
+        print("G Value - ", self.g)
+        print("Heuristic Distance - ",self.h)
+        print("F Value - " , self.f)
+        if self.visited == True:
+            print("This cell has been visited/processed")
+        else:
+            print("This cell has not been visited yet")
 
 
 class AStarResources():
@@ -24,8 +34,38 @@ class AStarResources():
         self.listOpen = []
         self.Values = Grid
         self.cellMatrix = self.Initiate_Maze()
-        self.startingCell = self.Get_Cell(1,1)
-        self.endingCell = None
+
+
+        validSelection = False
+        while validSelection == False:
+            print("Please enter a starting position")
+            userStartx = int(input("X Value: "))
+            userStarty = int(input("Y Value: "))
+            if self.Validate_Cell(self.Get_Cell(userStartx , userStarty)) == True:
+                validSelection = True
+                self.startingCell = self.Get_Cell(userStartx,userStarty)
+            else:
+                
+                print("The chosen starting cell was not valid")
+                print("Please choose another")
+
+
+                
+        validSelection = False
+        while validSelection == False:
+            print("Please enter an ending position")
+            userEndx = int(input("X Value: "))
+            userEndy = int(input("Y Value: "))
+            if self.Validate_Cell(self.Get_Cell(userEndx,userEndy)) == True:
+                validSelection = True
+                self.endingCell = self.Get_Cell(userEndx,userEndy)
+                self.endingCell.value = 2 
+                
+            else:
+                print("The chosen starting cell was not valid")
+                print("Please choose another")
+                
+        
         self.listOpen.append(self.startingCell)
         self.heuristicMethod = method
 
@@ -43,6 +83,7 @@ class AStarResources():
         return maze
     
     def Get_Neighbours(self,cell):
+        
         neighbourNorth = self.Get_Cell(cell.x,((cell.y)-1))
         neighbourEast = self.Get_Cell(((cell.x)+1),cell.y)
         neighbourSouth = self.Get_Cell(cell.x,((cell.y)+1))
@@ -50,13 +91,13 @@ class AStarResources():
         Neighbours = [neighbourNorth,neighbourEast,neighbourWest,neighbourSouth]
         return Neighbours
     def Get_Heuristics(self,cell):
+        xDifference = abs(cell.x - self.endingCell.x)
+        yDifference = abs(cell.y - self.endingCell.y)
         
-        xDifference = abs(cell.x - self.startingCell.x)
-        yDifference = abs(cell.y - self.startingCell.y)
-        if self.heuristicMethod == 1:
-            #euclidean distance
-            distance = ((xDifference**2)+(yDifference)**2)**0.5
         if self.heuristicMethod == 2:
+            #euclidean distance
+            distance = (((xDifference**2)+(yDifference)**2)**0.5) #<-- made a multiplier to get it on track
+        if self.heuristicMethod == 1:
             #manhattan distance
             distance = xDifference + yDifference
 
@@ -70,8 +111,8 @@ class AStarResources():
         return self.cellMatrix[y][x]
     
     def Update_Cell(self,cell,Parent):
-         cell.g = Parent.g + 10
-         cell.h = self.Get_Heuristics(Cell)
+         cell.g = Parent.g + 0.7
+         cell.h = self.Get_Heuristics(cell)
          cell.f =  cell.g + cell.h
          cell.parent = Parent
     
@@ -82,7 +123,7 @@ class AStarResources():
         pass
     
     def Find_Minimum(self,List):
-      #include the design for this recursive function 
+      #include the design for this recursive function
         if len(List)==2:
             if List[0].f < List[1].f:
                 return List[0]
@@ -91,44 +132,100 @@ class AStarResources():
         else:
             
             testvalue = self.Find_Minimum(List[1:])
-            if List[0].f < testvalue:
+            if List[0].f < testvalue.f:
                 return List[0]
             else:
                 return testvalue
+    def Validate_Cell(self,cell):
+        if cell.x > len(self.cellMatrix[1]):
+            
+            return False
+        elif cell.y > len(self.cellMatrix):
+            return False
+        elif cell.value != 0:
+            return False
+        elif cell.visited == True:
+            return False
+        else:
+            return True 
+        
     
 
     def Select_New_Cell(self):
-        minimumCell = Find_Minimum(self.openList)
+    
+        minimumCell = self.Find_Minimum(self.listOpen)
         return minimumCell
-        
+    
+    
     def Solve(self):
         Origin = self.startingCell
         found = False
         while found == False:
+            print("Start Origin =", Origin)
+            print("With an f value of " , Origin.f)
+            print("And a g value of ", Origin.g)
+            print("And an h value of " , Origin.h) 
+            print()
+                            
             if Origin == self.endingCell:
                 found = True
                 self.Get_Path()
                 return self.cellMatrix
                 break
-            Origin.visited = True
-            Origin.value = 3 #in order for the GUI to see a value is visited we must change its value to 3
+                print("End was found")
+           
             #0 = Unvisited open space
             #1 = Wall
             #2 = Finish
             #3 = Visited
             #4 = Edge
-            #Since we actually have attributes for the finish and whether a cell is visited, these values mainly for the use of the GUI 
+            #5 = Open List
+            #Since we actually have attributes for the finish and whether a cell is visited, these values mainly for the use of the GUI
+
             neighbours = self.Get_Neighbours(Origin)
+            #APPENDING VALID NEIGHBOURS TO OPEN LIST
+            print()
+            print()
+            print()
+            print("Evaluating Origins Neighbours")
             for Cell in neighbours:
-                if Cell.value == 0 and Cell.visited == False: #No point processing cells that are walls since we cant move to them anyway.
-                    #By doing this we increase efficiency
-                    self.Update_Cell(Cell,Origin) #sends target 'neighbour' cell and also orgin cell to be set as its parent
+                if Cell == self.endingCell:
+                    Cell.f = 0
                     self.listOpen.append(Cell)
-                    print("Appended Cell ", Cell.x, "," , Cell.y, "to the open list")
-            print("Open List now contains objects", self.listOpen)
+                else:
+                    
+                    if self.Validate_Cell(Cell) == True: #No point processing cells that are walls since we cant move to them anyway.
+                        #By doing this we increase efficiency
+                        print("Updating neighbours")
+                        self.Update_Cell(Cell,Origin) #sends target 'neighbour' cell and also origin cell to be set as its parent
+                        self.listOpen.append(Cell)
+                        Cell.value = 5
+                        print("Appended Cell ", Cell.x, "," , Cell.y, "to the open list")
+                    else:
+                        print("Cell ", Cell.x, "," , Cell.y, "was not valid")
+           
+           
+
+            #issue to talk about - the program kept selecting the starting cell as the new origin every time becuase we forgot to remove it from the open
+            #list. Since the cost function did not have any biases towards weight over cost. It chose the same cell every time 
+
+                    
+            Origin.visited = True
+            self.listOpen.remove(Origin)
+            Origin.value = 3 #in order for the GUI to see a value is visited we must change its value to 3
 
             Origin = self.Select_New_Cell()
             
+            print("The open list is",self.listOpen )
+            print("F VALUES")
+            for Cell in self.listOpen:
+                print("Cell ", Cell , " Has H Value " , Cell.h ,"Has G Value" , Cell.g , "Has F Value " , Cell.f)
+        
+
+
+            
+            #  Display_Result(self.Cell_Convert(self.cellMatrix))  
+                
             #Now after calculating heuristic we must choose the cell with the lowest f value as the new origin
                     
                 
@@ -136,7 +233,17 @@ class AStarResources():
             #origin = chosencell
 
             #RETURN THE FINISHED PATH AND THE GRID SO IT CAN BE DISPLAYED!!!!!
+
+
             
+def Cell_Convert(cellMatrix):
+        grid = []
+        for cell in cellMatrix:
+            row = []
+            for inner in cell:
+                row.append(inner.value)
+            grid.append(row) 
+        return grid           
 
 def Run_AStar_Tests():
     pass
@@ -200,11 +307,15 @@ def Close_Window(root):
     root.destroy()
 def Display_Result(Grid):
     root = Tk()
-    wall = tk.PhotoImage(file = "MazePiece_Wall.gif")
-    space = tk.PhotoImage(file = "MazePiece_Space.gif")
-    edge = tk.PhotoImage(file = "MazePiece_Outer.gif")
-    visited = tk.PhotoImage(file = "MazePiece_Visited.gif")
-    finish = tk.PhotoImage(file = "MazePiece_Finish.gif")
+    wall = tk.PhotoImage(file = "GUI_Image_Wall.gif")
+    space = tk.PhotoImage(file = "GUI_Image_Space.gif")
+    edge = tk.PhotoImage(file = "GUI_Image_Outer.gif")
+    visited = tk.PhotoImage(file = "GUI_Image_Visited.gif")
+    finish = tk.PhotoImage(file = "GUI_Image_Finish.gif")
+    processed = tk.PhotoImage(file = "GUI_Image_Processed.gif")
+    path = tk.PhotoImage(file = "GUI_Image_Path.gif")
+                           
+    
         
     for y in range(len(Grid)):
         for x in range(len(Grid[y])):
@@ -218,6 +329,8 @@ def Display_Result(Grid):
                 label = Label(root, image=visited , width = 30 , height = 30).grid(row = y , column = x)
             elif Grid[y][x] == 4:
                 label = Label(root, image=edge , width = 30 , height = 30).grid(row = y , column = x)
+            elif Grid[y][x] == 5:
+                label = Label(root, image=processed , width = 30 , height = 30).grid(row = y , column = x)
                 
     b = Button(root, text = "Next" , command = lambda: Close_Window(root)).grid(row = round((len(Grid[1])/2))  , column = (len(Grid)+1))
     root.lift()
@@ -256,8 +369,6 @@ def main():
 
        
         choice = None
-
-        
         while ValidateInteger(choice,1,2):
             print("1 = Run Program")
             print("2 = Run Tests") 
@@ -269,6 +380,7 @@ def main():
             AStar = AStarResources(Grid,method)
            # Path,Grid = Astar.Solve()
             Grid = AStar.Solve()
+            Display_Result(Cell_Convert(Grid))
 
             
         elif choice == 2:
