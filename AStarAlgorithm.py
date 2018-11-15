21d6dbc734cd738ca5c8ed7ececff846c082d2a1
OscarWilson <ozziew007@yahoo.co.uk>
2018-11-15 13:26:10 +0000

    Update AStarAlgorithm.py

--- AStarAlgorithm.py
+++ AStarAlgorithm.py
@@ -7,7 +7,6 @@ import heapq
 
 globalGridCapture = 0 
 class Cell():
-
     def __init__(self,x,y):
         self.value = 0
         self.parent = None
@@ -17,20 +16,48 @@ class Cell():
         self.h = 0
         self.f = 0
         self.visited = False
-
-
+    def Display_Details(self):
+        print("Cell ", self.x ,",", self.y )
+        print("Holds value - ", self.value)
+        print("G Value - ", self.g)
+        print("Heuristic Distance - ",self.h)
+        print("F Value - " , self.f)
+        if self.visited == True:
+            print("This cell has been visited/processed")
+        else:
+            print("This cell has not been visited yet")
 class AStarResources():
     def __init__(self,Grid,method):
-        
+        self.listOpen = []
         self.Values = Grid
         self.cellMatrix = self.Initiate_Maze()
-        self.startingCell = self.Get_Cell(1,1)
-        self.endingCell = None
+        validSelection = False
+        while validSelection == False:
+            print("Please enter a starting position")
+            userStartx = int(input("X Value: "))
+            userStarty = int(input("Y Value: "))
+            if self.Validate_Cell(self.Get_Cell(userStartx , userStarty)) == True:
+                validSelection = True
+                self.startingCell = self.Get_Cell(userStartx,userStarty)
+            else:   
+                print("The chosen starting cell is a wall and is invalid as a starting point")
+                print("Please choose another")
+        validSelection = False
+        while validSelection == False:
+            print("Please enter an ending position")
+            userEndx = int(input("X Value: "))
+            userEndy = int(input("Y Value: "))
+            if self.Validate_Cell(self.Get_Cell(userEndx,userEndy)) == True:
+                validSelection = True
+                self.endingCell = self.Get_Cell(userEndx,userEndy)
+                self.endingCell.value = 2 
+            else:
+                print("The chosen starting cell is a wall and is invalid as a starting point")
+                print("Please choose another") 
         self.listOpen.append(self.startingCell)
         self.heuristicMethod = method
-
-   
-                           
+        self.Values[self.endingCell.y][self.endingCell.x] = 2 
+        Display_Result(self.Values)            
     def Initiate_Maze(self):
         maze = []
         for y in range(len(self.Values)):
@@ -38,99 +65,123 @@ class AStarResources():
             for x in range(len(self.Values[y])):
                 newCell = Cell(x,y)#somebody check if I got the x and y the right way round
                 newCell.value = self.Values[y][x]
-                row.append(NewCell)
+                row.append(newCell)
             maze.append(row)
         return maze
-    
-    def Get_Neigbours():
-        pass
-
+    def Get_Neighbours(self,cell):
+        neighbourNorth = self.Get_Cell(cell.x,((cell.y)-1))
+        neighbourEast = self.Get_Cell(((cell.x)+1),cell.y)
+        neighbourSouth = self.Get_Cell(cell.x,((cell.y)+1))
+        neighbourWest = self.Get_Cell(((cell.x)-1),cell.y)
+        Neighbours = [neighbourNorth,neighbourEast,neighbourWest,neighbourSouth]
+        return Neighbours
     def Get_Heuristics(self,cell):
-        
-        xDifference = abs(cell.x - self.startingCell.x)
-        yDifference = abs(cell.y - self.startingCell.y)
-        if self.heuristicMethod == 1:
-            #euclidean distance
-            distance = ((xDifference**2)+(yDifference)**2)**0.5
+        xDifference = abs(cell.x - self.endingCell.x)
+        yDifference = abs(cell.y - self.endingCell.y)
         if self.heuristicMethod == 2:
+            #euclidean distance
+            distance = (((xDifference**2)+(yDifference)**2)**0.5) #<-- made a multiplier to get it on track
+        if self.heuristicMethod == 1:
             #manhattan distance
             distance = xDifference + yDifference
 
         return distance 
-            
-        
-       
-
     def Get_Cell(self, x, y):
         #returns a specified cell as its object
         #The reference to the cell memory is returned, remember in OOP
         #this means that after x = get_cell(x,y)changing x will change the cell
         return self.cellMatrix[y][x]
-    def Update_Cell(self,Cell,Parent):
-        # cell.g = origin.g + 10
-        # cell.h = get heuristic (cell)
-        # cell.f = sum of
-        # cell.parent = parent
-    
-        pass
+    def Update_Cell(self,cell,Parent):
+         cell.g = Parent.g + 0 #0.7
+         cell.h = self.Get_Heuristics(cell)
+         cell.f =  cell.g + cell.h
+         cell.parent = Parent
     def Get_Path(self):
-        #get end path 
+        #get end path
+        print("GETTING PATH")
+        #mark all pathpoints as cell.value = GUI_Image_Path
         pass
-
+    def Find_Minimum(self,List):
+      #include the design for this recursive function
+        if len(List)==1:
+            return List[0]
+        elif len(List)==2:
+            if List[0].f < List[1].f:
+                return List[0]
+            else:
+                return List[1]
+        else:
+            testvalue = self.Find_Minimum(List[1:])
+            if List[0].f < testvalue.f:
+                return List[0]
+            else:
+                return testvalue
+    def Validate_Cell(self,cell): # WHEN A STARTING POSITION IS ENTERED ABOVE THE LIMIT IT IS NOT PICKING IT UP  TO BE FIXED !!!
+        if cell.x > len(self.cellMatrix[1]):
+            return False
+        elif cell.y > len(self.cellMatrix):
+            return False
+        elif cell.value != 0:
+            return False
+        elif cell.visited == True:
+            return False
+        else:
+            return True 
     def Select_New_Cell(self):
-        #ngl i have no idea why i created this. i obviously had thought of something at the time
-        #I think this is the function that gets a new cell with the lowest f from open 
-        pass
-        
-
+        minimumCell = self.Find_Minimum(self.listOpen)
+        return minimumCell
     def Solve(self):
         Origin = self.startingCell
-        while found == False:
+        found = False
+        while found == False:          
             if Origin == self.endingCell:
                 found = True
                 self.Get_Path()
+                return self.cellMatrix
                 break
-            Origin.visited = True
-            Origin.value = 3 #in order for the GUI to see a value is visited we must change its value to 3
-            #0 = Unvisited open space
-            #1 = Wall
-            #2 = Finish
-            #3 = Visited
-            #4 = Edge
-            #Since we actually have attributes for the finish and whether a cell is visited, these values mainly for the use of the GUI 
-            neighbours = Get_Neighbours(Origin)
+                print("End was found")
+            neighbours = self.Get_Neighbours(Origin)
+            print(self.cellMatrix)
             for Cell in neighbours:
-                print("Processed Cell: ", Cell.x , Cell.y)
-                if Cell.value == 0 and Cell.visited == False: #No point processing cells that are walls since we cant move to them anyway.
-                    #By doing this we increase efficiency
-                    Update_Cell(Cell,Origin) #sends target 'neighbour' cell and also orgin cell to be set as its parent
+                if Cell == self.endingCell:
+                    Cell.f = 0
                     self.listOpen.append(Cell)
-                    print("Processed Cell: ", Cell.x ,",", Cell.y)
-                    print("Appended Cell ", Cell.x, "," , Cell.y, "to the open list")
-            print("Open List now contains objects", self.listOpen)
-            
-            #Now after calculating heuristic we must choose the cell with the lowest f value as the new origin
+                else:
                     
-                
-            #Choose Cell lowest F from open list using select new cell function .already made look up 
-            #origin = chosencell
-
+                    if self.Validate_Cell(Cell) == True: #No point processing cells that are walls since we cant move to them anyway.
+                        #By doing this we increase efficiency
+                        print("Updating neighbours")
+                        self.Update_Cell(Cell,Origin) #sends target 'neighbour' cell and also origin cell to be set as its parent
+                        self.listOpen.append(Cell)
+                        Cell.value = 5
+                    else:
+                        pass
+            print("ORIGIN: ", Origin.Display_Details)
+            print(self.listOpen)
+            #issue to talk about - the program kept selecting the starting cell as the new origin every time becuase we forgot to remove it from the open
+            #list. Since the cost function did not have any biases towards weight over cost. It chose the same cell every time           
+            Origin.visited = True
+            self.listOpen.remove(Origin)
+            Origin.value = 3 #in order for the GUI to see a value is visited we must change its value to 3
+            Origin = self.Select_New_Cell()
             #RETURN THE FINISHED PATH AND THE GRID SO IT CAN BE DISPLAYED!!!!!
-            
-
-def Run_AStar_Tests():
-    pass
-
         
+def Cell_Convert(cellMatrix):
+        grid = []
+        for cell in cellMatrix:
+            row = []
+            for inner in cell:
+                row.append(inner.value)
+            grid.append(row) 
+        return grid           
+def Run_AStar_Tests(): #DELETE THIS AND THE OPTION IN MAIN TO USE IT 
+    pass  
 def Move(Matrix,x,y):
-
-    
     if Matrix[y][x] == 2:
         global globalGridCapture
         globalGridCapture = Matrix
         print ('found at %d,%d' % (x, y)) 
-        return True
-        
+        return True     
     elif Matrix[y][x] == 1: 
         print ('wall at %d,%d' % (x, y)) 
         return False 
@@ -140,32 +191,19 @@ def Move(Matrix,x,y):
     elif Matrix[y][x] == 4: 
         print ('edge at %d,%d' % (x, y)) 
         return False
-    
     print ('visiting %d,%d' % (x, y)) 
     Matrix[y][x] = 3
-    
     if ((x < len(Matrix)-1 and Move(Matrix,x+1, y)) 
- 
         or (y > 0 and Move(Matrix,x, y-1)) 
-
         or (x > 0 and Move(Matrix,x-1, y)) 
-
         or (y < len(Matrix)-1 and Move(Matrix,x, y+1))): 
- 
         return True 
- 
-    return False
-
-
-           
+    return False          
 def ValidateInteger(userInput , intMin , intMax):
-    
     try:
         int(userInput)
-    except:
-        
+    except:  
         return True
-    
     userinput = int(userInput)
     if userinput < intMin:
         print("input was not valid")
@@ -175,41 +213,42 @@ def ValidateInteger(userInput , intMin , intMax):
         return True
     else:
         return False
-    
 def Close_Window(root):
     root.destroy()
 def Display_Result(Grid):
+    imageheight = 25
+    imagewidth = 25 
     root = Tk()
-    wall = tk.PhotoImage(file = "MazePiece_Wall.gif")
-    space = tk.PhotoImage(file = "MazePiece_Space.gif")
-    edge = tk.PhotoImage(file = "MazePiece_Outer.gif")
-    visited = tk.PhotoImage(file = "MazePiece_Visited.gif")
-    finish = tk.PhotoImage(file = "MazePiece_Finish.gif")
-        
+    wall = tk.PhotoImage(file = "GUI_Image_Wall.gif")
+    space = tk.PhotoImage(file = "GUI_Image_Space.gif")
+    edge = tk.PhotoImage(file = "GUI_Image_Outer.gif")
+    visited = tk.PhotoImage(file = "GUI_Image_Visited.gif")
+    finish = tk.PhotoImage(file = "GUI_Image_Finish.gif")
+    processed = tk.PhotoImage(file = "GUI_Image_Processed.gif")
+    path = tk.PhotoImage(file = "GUI_Image_Path.gif")  
     for y in range(len(Grid)):
         for x in range(len(Grid[y])):
             if Grid[y][x] == 0:
-                label = Label(root, image=space, width = 30 , height = 30).grid(row = y , column = x)
+                label = Label(root, image=space, width = imagewidth , height = imageheight).grid(row = y , column = x)
             elif Grid[y][x] == 1:
-                label = Label(root, image=wall , width = 30 , height = 30).grid(row = y , column = x)
+                label = Label(root, image=wall , width = imagewidth , height = imageheight).grid(row = y , column = x)
             elif Grid[y][x] == 2:
-                label = Label(root, image=finish , width = 30 , height = 30).grid(row = y , column = x)
+                label = Label(root, image=finish , width = imagewidth , height = imageheight).grid(row = y , column = x)
             elif Grid[y][x] == 3:
-                label = Label(root, image=visited , width = 30 , height = 30).grid(row = y , column = x)
+                label = Label(root, image=visited , width = imagewidth , height = imageheight).grid(row = y , column = x)
             elif Grid[y][x] == 4:
-                label = Label(root, image=edge , width = 30 , height = 30).grid(row = y , column = x)
-                
+                label = Label(root, image=edge , width = imagewidth , height = imageheight).grid(row = y , column = x)
+            elif Grid[y][x] == 5:
+                label = Label(root, image=processed , width = imagewidth , height = imageheight).grid(row = y , column = x)
     b = Button(root, text = "Next" , command = lambda: Close_Window(root)).grid(row = round((len(Grid[1])/2))  , column = (len(Grid)+1))
     root.lift()
     root.attributes('-topmost', True)
     root.attributes('-topmost', False)
-    root.mainloop()
-          
-
+    root.mainloop()        
 def main():
     print("Starting")
     gridStore = TestGrids.ImportMatrices()
-    Grid = gridStore.TestMaze4
+    Grid = gridStore.TestMaze1
     userInput = None
     while ValidateInteger(userInput,1,2):
         print()
@@ -218,13 +257,9 @@ def main():
         print("2 = Depth First Search")
         print("3 = Breadth First Search")
         userInput = input()
-    userInput = int(userInput)
-
-        
+    userInput = int(userInput)  
     #---A Star---#
     if userInput == 1:
-
-
         method = None
         while ValidateInteger(method,1,2):
             print("Please choose heuristic method:")
@@ -232,45 +267,34 @@ def main():
             print("2 = Euclidean")
             method = input()
         method = int(method)
-            
-
-       
         choice = None
-
-        
         while ValidateInteger(choice,1,2):
             print("1 = Run Program")
             print("2 = Run Tests") 
             choice = input()
         choice = int(choice)
-        
         if choice == 1:
             print("Initialising AStar")
             AStar = AStarResources(Grid,method)
-            Path,Grid = Astar.Solve()
-
-            
+           # Path,Grid = Astar.Solve()
+            Grid = AStar.Solve()
+            Display_Result(Cell_Convert(Grid))
         elif choice == 2:
-            Run_AStar_Tests()
-
-            
-
-        
+            Run_AStar_Tests()  
     #---Recursive Depth First---#
-    elif userInput == 2:
-        
-        
+    elif userInput == 2:       
+        StartPosx = int(input("Input Start Position X:"))
+        StartPosy = int(input("Input Start Position Y:"))
+        EndPosx = int(input("Input End Position X:"))
+        EndPosy = int(input("Input End Position Y:"))
+        Grid[EndPosy][EndPosx] = 2
         Display_Result(Grid)
-        StartPosx = 1
-        StartPosy = 1
+        
         Move(Grid , StartPosx , StartPosy)
         try:
             Display_Result(globalGridCapture)
         except:
             print("The finish could not be found or is inaccessible")
-        
-        
-    
     #---Breadth First---#
     elif userInput == 3:
         pass
@@ -323,18 +347,16 @@ main()
 ##        for y in range(len(self.maze)):
 ##            for x in range(len(self.maze[y])):
 ##                if self.maze[y][x] == 0:
-##                    label = Label(root, image=self.space, width = 30 , height = 30).grid(row = y , column = x)
+##                    label = Label(root, image=self.space, width = imageheight , height = imageheight).grid(row = y , column = x)
 ##                elif self.maze[y][x] == 1:
-##                    label = Label(root, image=self.wall , width = 30 , height = 30).grid(row = y , column = x)
+##                    label = Label(root, image=self.wall , width = imageheight , height = imageheight).grid(row = y , column = x)
 ##                elif self.maze[y][x] == 2:
-##                    label = Label(root, image=self.finish , width = 30 , height = 30).grid(row = y , column = x)
+##                    label = Label(root, image=self.finish , width = imageheight , height = imageheight).grid(row = y , column = x)
 ##                elif self.maze[y][x] == 3:
-##                    label = Label(root, image=self.visited , width = 30 , height = 30).grid(row = y , column = x)
+##                    label = Label(root, image=self.visited , width = imageheight , height = imageheight).grid(row = y , column = x)
 ##                elif self.maze[y][x] == 4:
-##                    label = Label(root, image=self.edge , width = 30 , height = 30).grid(row = y , column = x)
+##                    label = Label(root, image=self.edge , width = imageheight , height = imageheight).grid(row = y , column = x)
 ##
 
 
                     
-
-
